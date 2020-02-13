5b05e2620eb5d50e04b802d7c8266a1f6e21e1c9
Mahmood Nihad <m.zibari@gmail.com>
2020-02-12 22:36:43 -0600

    initialized index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+function renderProgress(){
+
+
+}
+
+function updateProgress(){
+
+
+}
+
+function renderQuestion(){
+
+
+}
+
+function renderOptions(){
+
+
+}
+
+function validateAnswer(){
+
+
+}
+
+function renderAnswer(){
+
+
+}
+
+function renderResults(){
+
+
+}
+
+function resetProgress(){
+
+    
+}
